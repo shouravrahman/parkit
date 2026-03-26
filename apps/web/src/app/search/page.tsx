267656af61a98f58760ccf3f9a8394c6e48a8b8c
shourav <shouravrahman006@gmail.com>
2026-03-26 21:58:06 +0600

    feat(page): add dynamic export for server-side rendering

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -3,6 +3,8 @@ import { SearchPage } from '@parkit/ui/src/components/templates/SearchPage'
 import { FormProviderSearchGarage } from '@parkit/forms/src/searchGarages'
 import { Container } from '@parkit/ui/src/components/atoms/Container'
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return (
     <Container>
