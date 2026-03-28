b580a3dd114979782ee3dc08a2a66aeb12736682
shourav <shouravrahman006@gmail.com>
2026-03-28 23:02:15 +0600

    feat: wrap LoginForm with Suspense in login pages across all applications

--- apps/web-manager/src/app/login/page.tsx
+++ apps/web-manager/src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import { LoginForm } from '@parkit/ui/src/components/templates/LoginForm'
 import { AuthLayout } from '@parkit/ui/src/components/molecules/AuthLayout'
+import { Suspense } from 'react'
 
 export default function Page() {
   return (
     <AuthLayout title={'Login'}>
-      <LoginForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </AuthLayout>
   )
 }
